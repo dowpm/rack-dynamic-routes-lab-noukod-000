f397c4efed97f6698b193b0e29e3bd795de74f01
Mac Dowell Prosper <macdowell@noukod.com>
2018-11-21 18:41:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,8 @@ class Application
    req = Rack::Request.new env
 
    if req.path.match(/items/)
-     item_price = req.path.split("/items/").last
-     item = @@item.select{|i| i.price == item_price}
+     item_name = req.path.split("/items/").last
+     item = @@item.select{|i| i.name == item_name}
 
      resp.write item.price
    else
