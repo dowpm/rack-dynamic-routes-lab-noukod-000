d0d890acbb55cb3f577ffb1bfe8202142eb6da81
Mac Dowell Prosper <macdowell@noukod.com>
2018-11-21 18:33:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,10 @@ class Application
    resp = Rack::Response.new
    req = Rack::Request.new env
 
-   if req.path.match(//)
+   if req.path.match(/items/)
 
    else
-     req.status = 404
+     resp.status = 404
    end
  end
 
