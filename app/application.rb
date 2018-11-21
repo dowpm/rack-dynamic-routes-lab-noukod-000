3253e3c066e93aef9e231f841a7838db675af1ab
Mac Dowell Prosper <macdowell@noukod.com>
2018-11-21 18:39:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,6 +8,8 @@ class Application
    if req.path.match(/items/)
      item_price = req.path.split("/items/").last
      item = @@item.find{|i| i.price == item_price}
+
+     resp.write item.price
    else
      resp.write "Route not found"
      resp.status = 404
